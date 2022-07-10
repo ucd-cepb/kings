36c5cf90f81f679b318576c2980e6845b5a26469
E.J. Zufall <90878994+ezufall@users.noreply.github.com>
2022-07-10 15:14:25 -0700

    adds dacplace gspshape overlaps

--- functions/create_spat_meta.R
+++ functions/create_spat_meta.R
@@ -8,6 +8,7 @@ library(sf)
 library(pbapply)
 library(ggplot2)
 library(scico)
+library(rcompanion)
 
 #type = "pop" or "area"
 create_svi_meta <- function(type){
@@ -188,19 +189,147 @@ create_dac_meta <- function(type, scope){
    median(dac_svi_tbl$SVI_percentile[dac_svi_tbl$DAC == F])
    
    ggplot(dac_svi_tbl, aes(x=DAC, y=SVI_percentile, fill=DAC)) +
-      geom_boxplot()+theme_minimal()+scale_fill_scico_d("roma")
-   #TODO SVI as function of (DAC | SDAC) 0/1 
-   #TODO SVI as 
-   #id formatting
-   #TODO SVI - DAC Mann-Whitney Test
-   
+      geom_boxplot()+theme_minimal()+scale_fill_scico_d(palette = "nuuk")
 
-   #TODO SVI - binned SDAC/DAC/NA test
-   gsp_svi_adjusted <- gsp_svi_adjusted %>% 
-      mutate(code = (as.character(gsp_ids)))%>% 
-      mutate(num_zeros = 4 - str_length(code)) %>% 
-      mutate(gsp_num_id = paste(ifelse(num_zeros > 0, "0", ""),ifelse(num_zeros > 1,"0",""),ifelse(num_zeros > 2, "0",""),code,sep = "")) %>% 
-      select(!c(code,num_zeros,gsp_ids))
+   #raw
+   ks.test(dac_svi_tbl$SVI_raw[dac_svi_tbl$DAC == T], "pnorm")
+   ks.test(dac_svi_tbl$SVI_raw[dac_svi_tbl$DAC == F], "pnorm")
+   #not a norm distr
+   ks.test(dac_svi_tbl$SVI_raw[dac_svi_tbl$DAC == F], 
+           dac_svi_tbl$SVI_raw[dac_svi_tbl$DAC == T], alternative = "greater")
+   
+   wt <- wilcox.test(dac_svi_tbl$SVI_raw, as.numeric(dac_svi_tbl$DAC))
+   wilcoxonRG(x = dac_svi_tbl$SVI_raw,
+              g = as.numeric(dac_svi_tbl$DAC) )
+   #null is rejected; SVI of DACs is higher
+   
+   median(dac_svi_tbl$SVI_raw[dac_svi_tbl$DAC == T])
+   median(dac_svi_tbl$SVI_raw[dac_svi_tbl$DAC == F])
+   
+   ggplot(dac_svi_tbl, aes(x=DAC, y=SVI_raw, fill=DAC)) +
+      geom_boxplot()+theme_minimal()+scale_fill_scico_d(palette = "nuuk")
+   
+   cor.test(as.numeric(dac_svi_tbl$DAC), dac_svi_tbl$SVI_raw, method = "pearson",
+            alternative = "greater")
+   
+   dac_svi_model <- lm(dac_svi_tbl$SVI_percentile ~ dac_svi_tbl$DAC)
+   summary(dac_svi_model)
+   apsrtable::apsrtable(dac_svi_model)
+
+   
+   
+   dacplace <- st_read("data_spatial_raw/dacs_2018/DAC_Pl18.shp")
+   dacplace <- st_transform(dacplace,albersNA)
+   dacplace <- st_make_valid(dacplace)
+ 
+   #census_gspshape_overs = st_intersects(censusplot, gsp_shapes)
+   gspshape_place_overs = st_intersects(gsp_shapes, dacplace)
+   gspshape_place_props = pblapply(seq_along(gspshape_place_overs),function(i){
+      #proportion of gsp in each census tract = area of census_gsp intersection / gsp area
+      area_overlap = st_area(st_intersection(gsp_shapes[i,],dacplace[gspshape_place_overs[[i]],]))
+      prop_gsp_in_place = area_overlap/st_area(gsp_shapes[i,])
+      prop_place_in_gsp = area_overlap/st_area(dacplace[gspshape_place_overs[[i]],])
+      data.table(place_id = dacplace$GEOID[gspshape_place_overs[[i]]],
+                 population = dacplace$Pop18[gspshape_place_overs[[i]]],
+                 DAC = ifelse(dacplace$DAC18[gspshape_place_overs[[i]]] == "Y", T, 
+                              ifelse(dacplace$DAC18[gspshape_place_overs[[i]]]=="N",F,NA)),
+                 Prop_GSP_in_place = as.numeric(prop_gsp_in_place),
+                 Prop_place_in_GSP = as.numeric(prop_place_in_gsp)
+                 #return entries where over half a percent of the place is in the GSP
+                 #as well as entries where over half a percent of the GSP is made up of that place
+      )[Prop_place_in_GSP >= 0.005 | Prop_GSP_in_place >= 0.005]
+   }, cl = 4)
+   
+   gsp_ids <- as.character(gsp_shapes$GSP.ID)
+
+   gsp_num_id = paste(ifelse((4-str_length(gsp_ids)) > 0, "0", ""),
+                      ifelse((4-str_length(gsp_ids)) > 1,"0",""),
+                      ifelse((4-str_length(gsp_ids)) > 2, "0",""),
+                      (gsp_ids),sep = "")
+   names(gspshape_place_props) = gsp_num_id
+ 
+   
+    gspshape_place_props[gsp_num_id[1]]
+   #each list item is a different gsp
+   
+   
+      #proportion of place in each census tract = area of census_dac intersection / dac area
+      area_overlap = st_area(st_intersection(dacplace[i,],censusplot[place_census_overs[[i]],]))
+      prop_place_in_tract = area_overlap/st_area(dacplace[i,])
+      prop_tract_in_place = area_overlap/st_area(censusplot[place_census_overs[[i]],])
+      data.table(census_tract_id = censusplot$FIPS[place_census_overs[[i]]],
+                 dac_place_id = dacplace$GEOID[i],
+                 population = censusplot$E_TOTPOP[place_census_overs[[i]]],
+                 dacpop = dacplace$Pop18[i],
+                 SVI_raw = ifelse(censusplot$SPL_THEMES[place_census_overs[[i]]]>=0,
+                                  censusplot$SPL_THEMES[place_census_overs[[i]]],NA),
+                 SVI_percentile = ifelse(censusplot$RPL_THEMES[place_census_overs[[i]]]>=0,
+                                         censusplot$RPL_THEMES[place_census_overs[[i]]],NA),
+                 DAC = ifelse(dacplace$DAC18[i] == "Y", T, 
+                              ifelse(dacplace$DAC18[i]=="N",F,NA)),
+                 Prop_place_in_tract = as.numeric(prop_place_in_tract),
+                 Prop_tract_in_place = as.numeric(prop_tract_in_place)
+                 #return entries where over 0.5 percent of the census tract is in the dac tract
+                 #as well as entries where over 0.5 percent of the dac tract is made up of the census tract
+      )[Prop_tract_in_place >= 0.005 | Prop_place_in_tract >= 0.005]
+   }, cl = 4)
+   
+   dac_svi_tbl <- list.rbind(dac_census_props)
+   saveRDS(dac_svi_tbl, "data_output/dac_svi_tbl")
+   
+   dac_svi_tbl <- dac_svi_tbl[!is.na(dac_svi_tbl$DAC) & 
+                                 !is.na(dac_svi_tbl$SVI_percentile)]
+   
+   
+   #TODO which tracts are DAC and SDAC?
+   
+   
+   #normality test shows data is non-normal
+   set.seed(0)
+   #shapiro.test(dac_svi_tbl$SVI_percentile[dac_svi_tbl$DAC == T])
+   ks.test(dac_svi_tbl$SVI_percentile[dac_svi_tbl$DAC == T], "pnorm")
+   ks.test(dac_svi_tbl$SVI_percentile[dac_svi_tbl$DAC == F], "pnorm")
+   #not a norm distr
+   ks.test(dac_svi_tbl$SVI_percentile[dac_svi_tbl$DAC == F], 
+           dac_svi_tbl$SVI_percentile[dac_svi_tbl$DAC == T], alternative = "greater")
+   
+   wilcox.test(dac_svi_tbl$SVI_percentile, as.numeric(dac_svi_tbl$DAC))
+   #null is rejected; SVI of DACs is higher
+   
+   median(dac_svi_tbl$SVI_percentile[dac_svi_tbl$DAC == T])
+   median(dac_svi_tbl$SVI_percentile[dac_svi_tbl$DAC == F])
+   
+   ggplot(dac_svi_tbl, aes(x=DAC, y=SVI_percentile, fill=DAC)) +
+      geom_boxplot()+theme_minimal()+scale_fill_scico_d(palette = "nuuk")
+   
+   #raw
+   ks.test(dac_svi_tbl$SVI_raw[dac_svi_tbl$DAC == T], "pnorm")
+   ks.test(dac_svi_tbl$SVI_raw[dac_svi_tbl$DAC == F], "pnorm")
+   #not a norm distr
+   ks.test(dac_svi_tbl$SVI_raw[dac_svi_tbl$DAC == F], 
+           dac_svi_tbl$SVI_raw[dac_svi_tbl$DAC == T], alternative = "greater")
+   
+   wt <- wilcox.test(dac_svi_tbl$SVI_raw, as.numeric(dac_svi_tbl$DAC))
+   wilcoxonRG(x = dac_svi_tbl$SVI_raw,
+              g = as.numeric(dac_svi_tbl$DAC) )
+   #null is rejected; SVI of DACs is higher
+   
+   median(dac_svi_tbl$SVI_raw[dac_svi_tbl$DAC == T])
+   median(dac_svi_tbl$SVI_raw[dac_svi_tbl$DAC == F])
+   
+   ggplot(dac_svi_tbl, aes(x=DAC, y=SVI_raw, fill=DAC)) +
+      geom_boxplot()+theme_minimal()+scale_fill_scico_d(palette = "nuuk")
+   
+   cor.test(as.numeric(dac_svi_tbl$DAC), dac_svi_tbl$SVI_raw, method = "pearson",
+            alternative = "greater")
+   
+   dac_svi_model <- lm(dac_svi_tbl$SVI_percentile ~ dac_svi_tbl$DAC)
+   summary(dac_svi_model)
+   apsrtable::apsrtable(dac_svi_model)
+   
+   
+   
+   
    
    saveRDS(gsp_svi_adjusted, file = paste0("data_output/","gsp_svi_",type,"_",format(Sys.time(), "%Y%m%d-%H:%M")))
    
