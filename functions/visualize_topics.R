5c3bd5d5ba8d93810dca942c53eb973d6a02fb8a
E.J. Zufall <90878994+ezufall@users.noreply.github.com>
2022-07-22 11:09:59 -0700

    added compare_models and visualize_topics functions

---
+++ functions/visualize_topics.R
@@ -0,0 +1,106 @@
+visualize_topics <- function(model, inputs, text_col){
+   library(ggplot2)
+   library(ggrepel)
+   library(scico)
+   numTopics = model$settings$dim$K
+   labels <- labelTopics(model, topics = c(1:numTopics), n = 10)
+   #sageLabels can be used when the model has a content covariate
+   #both print highest probability words and FREX words associated with each topic
+   #FREX is weighted by frequency and exclusivity
+   #lift() weights words higher if they have lower frequency in other topics
+   #score() is similar, but log based -- see lda package
+   
+   #inspect words associated with topics using labelTopics
+   for(i in 1:numTopics){
+      print(paste0("Top 10 FREX Words in Topic ", i, ":", collapse = ""))
+      print(labels$frex[i,])
+   }
+   
+   #TODO gratitude to francescoaberlin.blog for this tutorial
+   #topics are evaluated on two components:
+   #semantic coherence (frequency of co-occurrence of common words in a toipc)
+   #exclusivity of words to topic
+   m5_ex_sem<-as.data.frame(cbind(c(1:numTopics),
+                                 exclusivity(model), 
+                                 semanticCoherence(model=model, 
+                                                   documents = inputs$documents), "model 5"))
+   
+   #can compare multiple models by adding to rbind
+   models_ex_sem<-rbind(m5_ex_sem)
+   
+   colnames(models_ex_sem)<-c("K","exclusivity", "semantic_coherence", "model")
+   models_ex_sem$exclusivity<-as.numeric(as.character(models_ex_sem$exclusivity))
+   models_ex_sem$semantic_coherence<-as.numeric(as.character(models_ex_sem$semantic_coherence))
+   
+   options(repr.plot.width=7, repr.plot.height=7, repr.plot.res=100)
+   
+   topic_qual_plot<-ggplot(models_ex_sem, aes(semantic_coherence, 
+                                              exclusivity, color = model))+
+      geom_point(size = 2, alpha = 1) + 
+      geom_text_repel(aes(label=K), nudge_x=.005, nudge_y=.005, 
+                      size = 2.5, alpha = 0.6, force = 0.8, force_pull = 1.5,
+                      max.overlaps = Inf)+
+      labs(x = "Semantic coherence",
+           y = "Exclusivity",
+           title = "Comparing exclusivity and semantic coherence among model topics")+
+      scale_color_scico_d(palette = "nuuk")+
+      theme_minimal()+theme(plot.title = element_text(hjust = 0.5))
+   topic_qual_plot
+  ggsave("topic_quality.png",plot = topic_qual_plot, device = "png", path = "figures",
+         width = 4020, height = 1890, dpi = 300, units = "px", bg = "white")
+   
+   
+   dim(readPNG("figures/topicqual_repel2.png"))
+   dim(readPNG("figures/topic_quality"))
+   
+   text_vect <- unlist(text_col)
+   #testing if text_col includes docs.removed and removes them if applicable
+   if (length(text_vect) != length(inputs$documents)){
+      text_vect <- text_vect[-inputs$docs.removed]
+   }
+   
+   #findThoughts example
+   thoughts <- findThoughts(model, texts = text_vect, topics = c(1:numTopics), n = 3)
+   topics = c(1:numTopics)
+   #plotQuote is a graphical wrapper to help present documents as examples
+   #example:
+   thought_5 <- findThoughts(model, texts = text_vect, topics = 5, n = 3)
+   plotQuote(thought_5, width = 30, main = "Topic 5")
+   
+   #TODO clean this up
+   plot.STM(gsp_model_saved, type = "labels", topics = c(4,6))
+   plot.STM(gsp_model_saved, type = "perspective", topics = c(48, 41))
+   plot(gsp_model_saved,type = "labels", topics = c(1:4))
+   plot(gsp_model_saved,type = "perspectives", topics = c(1,40))
+   plot(gsp_model_saved, type = "summary", xlim = c(0,0.3))
+   
+   #cloud has poor performance: only plots a few words
+   #cloud(gsp_model_saved,documents = gsp_out$documents)
+   
+
+   theta <- as_tibble(gsp_model_saved$theta)
+   tags <- rep(NA, nrow(theta))
+   for(i in 1:numTopics){
+      for(j in 1:nrow(theta)){
+         if(theta[[i]][j]>=0.7){
+            tags[j] <- paste0("topic",i,collapse = "")
+         }
+      }
+   }
+   
+   #look at the relationship between metadata and topics
+   effect <- estimateEffect(1:50 ~ admin + 
+                             basin_plan +
+                             sust_criteria +
+                             monitoring_networks + 
+                             projects_mgmt_actions +
+                             SVI_na_adj+
+                             as.factor(approval) +
+                             as.factor(priority)+ 
+                             ag_gw_asfractof_tot_gw, 
+                          model,
+                          meta = inputs$meta, uncertainty = "None")
+   summary(effect, topics = c(1:50))
+   
+   #calculate topic correlations: topicCorr
+}
--- gsp_topic_analyzer.R
+++ gsp_topic_analyzer.R
@@ -10,11 +10,14 @@ library(geometry)
 library(Rtsne)
 library(rsvd)
 library(stringi)
+library(wordcloud)
 
 source('functions/lex_clean.R')
 source('functions/create_lang_meta.R')
 source('functions/create_spat_meta.R')
 source('functions/generate_place_names.R')
+source('functions/compare_models.R')
+source('functions/visualize_topics.R')
 
 gsp_text_with_lang <- create_lang_meta()
 
@@ -52,34 +55,30 @@ saveRDS(gsp_text_with_meta, file = paste0("data_output/","gsp_docs_w_meta_",form
 gsp_text_with_meta <- readRDS(
    list.files(path = "data_output",pattern = "meta", full.names = T)[length(
       list.files(path = "data_output", pattern = "meta", full.names = T))])
+
 gsp_out <- lex_clean(gsp_text_with_meta)
 
+gsp_out <- readRDS(list.files(path = "data_temp", pattern = "slam", full.names = T)[length(
+   list.files(path = "data_temp", pattern = "slam", full.names = T))])
 
-#test svi for both
-#test k = 5, 10, 20, 40, 80
-#cut bad characters
-#drop anything that doesn't have a letter
-#regex or name density check against USGS placenames database
-#or regex for terrible matches and outputs a stopword matrix
-#figure out how often topic 9 words show up on the document term matrix (grep) row names for where equals > 0
-#prevalence = how often topic is discussed. category vars included here. svi included.
-#content = word frequency within topic. allows one categorical var, advised to be small
+selected_model <- compare_models(optimize_K = T, obj = gsp_out)
 
+numTopics = selected_model$settings$dim$K
+#or set to a specific value such as
+#numTopics = 50
 
 gsp_model <- stm(documents = gsp_out$documents, vocab = gsp_out$vocab,
-                 K = 50, prevalence =~ admin + 
+                 K = numTopics, prevalence =~ admin + 
                     basin_plan +
                     sust_criteria +
                     monitoring_networks + 
                     projects_mgmt_actions + 
-                    as.factor(gsp_id) +
                     SVI_na_adj+
                     as.factor(approval)+
                     as.factor(priority)+
                     ag_gw_asfractof_tot_gw,
-                 max.em.its = 120,
+                 max.em.its = 150,
                  data = gsp_out$meta, init.type = "Spectral")  
-
 #dummy for how many gsas are involved: multiple or one
 
 saveRDS(gsp_model, file = paste0("data_output/","model_",format(Sys.time(), "%Y%m%d-%H:%M")))
@@ -87,77 +86,10 @@ saveRDS(gsp_model, file = paste0("data_output/","model_",format(Sys.time(), "%Y%
 gsp_model_saved <- readRDS(list.files(path = "data_output", pattern = "model", full.names = T)[length(
    list.files(path = "data_output", pattern = "model", full.names = T))])
 
-
-#inspect words associated with topics using labelTopics
-labelTopics(gsp_model_saved, c(1:50))
-
-#example:
-#how to let searchK figure out how many topics to generate
-storage <- searchK(gsp_out$documents, gsp_out$vocab, K = c(7, 10),
-                   + prevalence =~ rating + s(day), data = meta)
-
-#example:
-#   see page 9 of stm documentation
-#max iterations = max.em.its
-#highly recommend init.type = "Spectral" because it eliminates initiation-
-#based sensitivity
-
-
-#topics are evaluated on two components:
-#semantic coherence (frequency of co-occurrence of common words in a toipc)
-#exclusivity of words to topics
-
-#uses selectModels object
-#example:
-#how to plot quality of models
-plotModels(poliblogSelect, pch = c(1, 2, 3, 4),
-           + legend.position = "bottomright")
-#can also use topicQuality
-
-#choose your favorite model
-selectedmodel <- poliblogSelect$runout[[3]]
-
-
-#sageLabels can be used when the model has a content covariate
-#both print highest probability words and FREX words associated with each topic
-#FREX is weighted by frequency and exclusivity
-#lift() weights words higher if they have lower frequency in other topics
-#score() is similar, but log based -- see lda package
-
-#findThoughts example
-#thoughts3 <- findThoughts(poliblogPrevFit, texts = shortdoc, n = 2,
-#+ topics = 6)$docs[[1]]
-#par example
-#par(mfrow = c(1, 2), mar = c(0.5, 0.5, 1, 0.5))
-
-
-#look at the relationship between metadata and topics
-#estimateEffect
-#call summary on estimateEffect object to print summary
-
-#example:
-#out$meta$rating <- as.factor(out$meta$rating)
-#prep <- estimateEffect(1:20 ~ rating + s(day), poliblogPrevFit,
-                          #+ meta = out$meta, uncertainty = "Global")
-#summary(prep, topics = 1)
-
-
-#plot(...,type = "labels"),
-
-#plot(...,type = "perspectives"), 
-
-#plot(poliblogPrevFit, type = "summary", xlime = c(0,0.3))
-
-#cloud function plots a word cloud
-
-#plotQuote is a graphical wrapper so you can present documents as examples
-#example
-#plotQuote(thoughts3, width = 30, main = "Topic 6")
-
-
-#calculate topic correlations: topicCorr
-
-
+visualize_topics(gsp_model_saved, 
+                 gsp_out, 
+                 gsp_text_with_meta$text[gsp_text_with_meta$is_comment == F &
+                                       gsp_text_with_meta$is_reference == F])
 
 #example of loading workspace with model loaded to reduce compile time
 load(results.rda)
