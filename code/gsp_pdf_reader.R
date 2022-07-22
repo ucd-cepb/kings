b6e3a4870fb7615897a53f513de1b249d1eb12bd
E.J. Zufall <90878994+ezufall@users.noreply.github.com>
2022-07-22 15:15:48 -0700

    began setting up box sync functionality

--- code/gsp_pdf_reader.R
+++ code/gsp_pdf_reader.R
@@ -1,37 +1,39 @@
-#turns each pdf into an array with each page as one line
-#these are saved as RDS files
-#saves a data table for each pdf with its plan element metadata as RDS files
-
-library(stm)
-library(pdftools)
-
-source('code/functions/create_page_key.R')
-
-pdfs <- list.files(path = "data_raw/portal", pattern = "pdf", full.names = T)
-#pdf texts 1-112 successfully saved
-#category texts 1-112 successfully saved
-for(k in 1:length(pdfs)){
-   #if pdf doesn't exist, read in and save pdf text
-   if(!file.exists(paste0("data_cleaned/",substr(pdfs[k],10,24),"_text"))){
-      text <- pdf_text(pdfs[[k]])
-      print(k)
-      #TODO check for blank pages and use ocr on them
-      for(i in 1:length(text)){
-         if (nchar(text[i])> 10000){
-            text[i] <- NA
+gsp_pdf_reader<- function(box_sync = F){
+   #turns each pdf into an array with each page as one line
+   #these are saved as RDS files
+   #saves a data table for each pdf with its plan element metadata as RDS files
+   
+   library(stm)
+   library(pdftools)
+   
+   source('code/functions/create_page_key.R')
+   
+   pdfs <- list.files(path = "data_raw/portal", pattern = "pdf", full.names = T)
+   #pdf texts 1-112 successfully saved
+   #category texts 1-112 successfully saved
+   for(k in 1:length(pdfs)){
+      #if pdf doesn't exist, read in and save pdf text
+      if(!file.exists(paste0("data_cleaned/",substr(pdfs[k],10,24),"_text"))){
+         text <- pdf_text(pdfs[[k]])
+         print(k)
+         #TODO check for blank pages and use ocr on them
+         for(i in 1:length(text)){
+            if (nchar(text[i])> 10000){
+               text[i] <- NA
+            }
          }
+         saveRDS(text, file = paste0("data_cleaned/",substr(pdfs[k],10,24),"_text"))
+
+         page_key <- create_page_key(paste0(substr(pdfs[[k]],1,24),".xlsx"))
+         saveRDS(page_key, file = paste0("data_cleaned/",substr(pdfs[k],10,24),"_categories"))
+         
       }
-      saveRDS(text, file = paste0("data_cleaned/",substr(pdfs[k],10,24),"_text"))
-      #load gsp_xls_cleaner functions
-      page_key <- create_page_key(paste0(substr(pdfs[[k]],1,24),".xlsx"))
-      saveRDS(page_key, file = paste0("data_cleaned/",substr(pdfs[k],10,24),"_categories"))
       
    }
-      
+   
+   
+   #testocr <- pdf_ocr_text(file.path("data_raw/portal/pdftools.pdf"))
+   #pdf_fonts() can find sections
 }
 
-
-#testocr <- pdf_ocr_text(file.path("data_raw/portal/pdftools.pdf"))
-
-#TODO pdf_fonts to find sections
 
--- code/gsp_topic_analyzer.R
+++ code/gsp_topic_analyzer.R
@@ -1,95 +1,111 @@
-library(stm)
-library(tm)
-library(SnowballC)
-library(tidytext)
-library(data.table)
-library(tidyverse)
-library(sf)
-library(pbapply)
-library(geometry)
-library(Rtsne)
-library(rsvd)
-library(stringi)
-library(wordcloud)
+gsp_topic_analyzer<- function(box_sync = F, build_meta = F, clean_lex = T, 
+                              model_compare = F, run_model = T, viz_results = T, 
+                              ntopics = NA){
+   library(stm)
+   library(tm)
+   library(SnowballC)
+   library(tidytext)
+   library(data.table)
+   library(tidyverse)
+   library(sf)
+   library(pbapply)
+   library(geometry)
+   library(Rtsne)
+   library(rsvd)
+   library(stringi)
+   library(wordcloud)
+   
+   source('code/functions/lex_clean.R')
+   source('code/functions/create_lang_meta.R')
+   source('code/functions/create_spat_meta.R')
+   source('code/functions/generate_place_names.R')
+   source('code/functions/compare_models.R')
+   source('code/functions/visualize_topics.R')
+   
+   if(build_meta==T){
+      gsp_text_with_lang <- create_lang_meta()
+      
+      #retrieves the latest save of gsp_text_with_lang
+      #generated in create_lang_meta, which allows create_lang_meta() to be skipped
+      
+      gsp_text_with_lang <- readRDS(
+         list.files(path = "data_output",pattern = "lang", full.names = T)[length(
+            list.files(path = "data_output", pattern = "lang", full.names = T))])
+      
+      
+      type = "area"
+      #or type = "pop"
+      gsp_svi_adjusted <- create_svi_meta(type)
+      
+      #retrieves lates save of gsp_svi_adjusted of the given type, which allows
+      #create_svi_meta to be skipped
+      gsp_svi_adjusted <- readRDS(
+         list.files(path = "data_output",pattern = type,full.names = T)[length(
+            list.files(path = "data_output",pattern = type,full.names = T))])
+      
+      
+      #rows = num docs; cols = metadata types
+      
+      #gsp_meta <- data.table(matrix(ncol = 4, nrow = 0))
+      #colnames(gsp_meta) <- c("GSA","community_attributes","ag_importance","soc_vuln")
+      
+      gsp_text_with_meta <- full_join(gsp_text_with_lang, gsp_svi_adjusted, by = c("gsp_id"="gsp_num_id"))
+      #filtering NA admin = proxy for GSPs whose texts have not been processed
+      gsp_text_with_meta <- gsp_text_with_meta %>% filter(!is.na(admin))
+      
+      saveRDS(gsp_text_with_meta, file = paste0("data_output/","gsp_docs_w_meta_",format(Sys.time(), "%Y%m%d-%H:%M")))
+      
+   }
+   
+   #retrieves the latest save of gsp_text_with_meta
+   gsp_text_with_meta <- readRDS(
+      list.files(path = "data_output",pattern = "meta", full.names = T)[length(
+         list.files(path = "data_output", pattern = "meta", full.names = T))])
+   
+   if(clean_lex == T){
+      gsp_out <- lex_clean(gsp_text_with_meta)
+      
+   }
+   
+   gsp_out <- readRDS(list.files(path = "data_temp", pattern = "slam", full.names = T)[length(
+      list.files(path = "data_temp", pattern = "slam", full.names = T))])
+   
+   if(model_compare==T){
+      selected_model <- compare_models(optimize_K = T, obj = gsp_out)
+      numTopics = selected_model$settings$dim$K
+   }else{
+      numTopics = ntopics
+   }
 
-source('code/functions/lex_clean.R')
-source('code/functions/create_lang_meta.R')
-source('code/functions/create_spat_meta.R')
-source('code/functions/generate_place_names.R')
-source('code/functions/compare_models.R')
-source('code/functions/visualize_topics.R')
-
-gsp_text_with_lang <- create_lang_meta()
-
-#retrieves the latest save of gsp_text_with_lang
-#generated in create_lang_meta, which allows create_lang_meta() to be skipped
-
-gsp_text_with_lang <- readRDS(
-   list.files(path = "data_output",pattern = "lang", full.names = T)[length(
-      list.files(path = "data_output", pattern = "lang", full.names = T))])
-
-
-type = "area"
-#or type = "pop"
-gsp_svi_adjusted <- create_svi_meta(type)
-
-#retrieves lates save of gsp_svi_adjusted of the given type, which allows
-#create_svi_meta to be skipped
-gsp_svi_adjusted <- readRDS(
-   list.files(path = "data_output",pattern = type,full.names = T)[length(
-      list.files(path = "data_output",pattern = type,full.names = T))])
-
-
-#rows = num docs; cols = metadata types
-
-#gsp_meta <- data.table(matrix(ncol = 4, nrow = 0))
-#colnames(gsp_meta) <- c("GSA","community_attributes","ag_importance","soc_vuln")
-
-gsp_text_with_meta <- full_join(gsp_text_with_lang, gsp_svi_adjusted, by = c("gsp_id"="gsp_num_id"))
-#filtering NA admin = proxy for GSPs whose texts have not been processed
-gsp_text_with_meta <- gsp_text_with_meta %>% filter(!is.na(admin))
-
-saveRDS(gsp_text_with_meta, file = paste0("data_output/","gsp_docs_w_meta_",format(Sys.time(), "%Y%m%d-%H:%M")))
-
-#retrieves the latest save of gsp_text_with_meta
-gsp_text_with_meta <- readRDS(
-   list.files(path = "data_output",pattern = "meta", full.names = T)[length(
-      list.files(path = "data_output", pattern = "meta", full.names = T))])
-
-gsp_out <- lex_clean(gsp_text_with_meta)
-
-gsp_out <- readRDS(list.files(path = "data_temp", pattern = "slam", full.names = T)[length(
-   list.files(path = "data_temp", pattern = "slam", full.names = T))])
-
-selected_model <- compare_models(optimize_K = T, obj = gsp_out)
-
-numTopics = selected_model$settings$dim$K
-#or set to a specific value such as
-#numTopics = 50
-
-gsp_model <- stm(documents = gsp_out$documents, vocab = gsp_out$vocab,
-                 K = numTopics, prevalence =~ admin + 
-                    basin_plan +
-                    sust_criteria +
-                    monitoring_networks + 
-                    projects_mgmt_actions + 
-                    SVI_na_adj+
-                    as.factor(approval)+
-                    as.factor(priority)+
-                    ag_gw_asfractof_tot_gw,
-                 max.em.its = 150,
-                 data = gsp_out$meta, init.type = "Spectral")  
-#dummy for how many gsas are involved: multiple or one
-
-saveRDS(gsp_model, file = paste0("data_output/","model_",format(Sys.time(), "%Y%m%d-%H:%M")))
-
-gsp_model_saved <- readRDS(list.files(path = "data_output", pattern = "model", full.names = T)[length(
-   list.files(path = "data_output", pattern = "model", full.names = T))])
-
-visualize_topics(gsp_model_saved, 
-                 gsp_out, 
-                 gsp_text_with_meta$text[gsp_text_with_meta$is_comment == F &
-                                       gsp_text_with_meta$is_reference == F])
-
-#example of loading workspace with model loaded to reduce compile time
-load(results.rda)
+   if(run_model == T){
+      gsp_model <- stm(documents = gsp_out$documents, vocab = gsp_out$vocab,
+                       K = numTopics, prevalence =~ admin + 
+                          basin_plan +
+                          sust_criteria +
+                          monitoring_networks + 
+                          projects_mgmt_actions + 
+                          SVI_na_adj+
+                          as.factor(approval)+
+                          as.factor(priority)+
+                          ag_gw_asfractof_tot_gw,
+                       max.em.its = 150,
+                       data = gsp_out$meta, init.type = "Spectral")  
+      #dummy for how many gsas are involved: multiple or one
+      
+      saveRDS(gsp_model, file = paste0("data_output/","model_",format(Sys.time(), "%Y%m%d-%H:%M")))
+      
+   }
+   
+   gsp_model_saved <- readRDS(list.files(path = "data_output", pattern = "model", full.names = T)[length(
+      list.files(path = "data_output", pattern = "model", full.names = T))])
+   
+   if(viz_topics == T){
+      visualize_topics(gsp_model_saved, 
+                       gsp_out, 
+                       gsp_text_with_meta$text[gsp_text_with_meta$is_comment == F &
+                                                  gsp_text_with_meta$is_reference == F])
+      
+   }
+   
+   return(gsp_model_saved)
+}
