32c2bad24887878daa4280aa0dde05e818fb2dce
E.J. Zufall <90878994+ezufall@users.noreply.github.com>
2022-08-22 15:17:08 -0700

    changed gsa to gsp grouping

--- code/summarize_pws_by_gsa.R
+++ code/summarize_pws_by_gsa.R
@@ -8,12 +8,11 @@ library(lubridate)
 pws <- st_read('data_temp/SABL_Public_220819')
 td <- tempdir()
 tf <- tempfile(tmpdir = td)
-download.file('https://sgma.water.ca.gov/portal/service/gsadocument/submittedgsa',destfile = tf)
-unzip(zipfile = tf,exdir = td)
-gsa <- st_read(grep('GSA.*shp$',list.files(td,full.names = T),value = T))
-gsa <- st_transform(gsa,st_crs(pws))
-pws_in_gsa <- st_intersects(pws,gsa)
-pws <- pws[sapply(pws_in_gsa,length)>0,]
+fpath = file.path("data_spatial_raw/GSP_submitted", grep('shp$',fname$Name,value=T))
+gsp <- st_read(fpath)
+gsp <- st_transform(gsp,st_crs(pws))
+pws_in_gsp <- st_intersects(pws,gsp)
+pws <- pws[sapply(pws_in_gsp,length)>0,]
 pws <- st_union(pws,by_feature = T,is_coverage = T)
 pws <- pws[pws$FEDERAL_CL %in% c('COMMMUNITY','NON-TRANSIENT NON-COMMUNITY'),]
 
@@ -37,25 +36,27 @@ pws <- pws[order(pws$BOUNDARY_T,decreasing = T),]
 pws <- pws[!duplicated(pws$SABL_PWSID),]
 
 pws <- st_simplify(pws)
-gsa <- st_simplify(gsa)
-gsa_inters_pws <- st_intersection(gsa,pws)
-gsa_inters_pws$area <- st_area(gsa_inters_pws)
+gsp <- st_simplify(gsp)
+#intersection adopts same structure as gsp
+gsp_inters_pws <- st_intersection(gsp,pws)
+gsp_inters_pws$area <- st_area(gsp_inters_pws)
 pws$area <- st_area(pws)
 
 ### some places (e.g., livermore) have jurisidictional boundary and
 # service boundary as separate rows. this keeps service boundary if available
-gsa_inters_pws$prop_of_pws <- round(as.numeric(gsa_inters_pws$area/pws$area[match(gsa_inters_pws$SABL_PWSID,pws$SABL_PWSID)]),2)
-gsa_inters_pws$service_count_prop <- gsa_inters_pws$SERVICE_CO*gsa_inters_pws$prop_of_pws
-gsa_inters_pws$HVIOL_PROP <- gsa_inters_pws$HEALTH_VIOL_COUNT_2014_2018*gsa_inters_pws$prop_of_pws
-gsa_inters_pws$NHVIOL_PROP <- gsa_inters_pws$NONHEALTH_VIOL_COUNT_2014_2018*gsa_inters_pws$prop_of_pws
+#service_co = number of service connections
+gsp_inters_pws$prop_of_pws <- round(as.numeric(gsp_inters_pws$area/pws$area[match(gsp_inters_pws$SABL_PWSID,pws$SABL_PWSID)]),2)
+gsp_inters_pws$service_count_prop <- gsp_inters_pws$SERVICE_CO*gsp_inters_pws$prop_of_pws
+gsp_inters_pws$HVIOL_PROP <- gsp_inters_pws$HEALTH_VIOL_COUNT_2014_2018*gsp_inters_pws$prop_of_pws
+gsp_inters_pws$NHVIOL_PROP <- gsp_inters_pws$NONHEALTH_VIOL_COUNT_2014_2018*gsp_inters_pws$prop_of_pws
 
-gsa_inters_pws$PRIMARY_SOURCE <- ifelse(grepl('^G',gsa_inters_pws$PRIMARY_SOURCE),'GW','SW')
-#### this creates a weighted count of violations and of people served by GSA
+gsp_inters_pws$PRIMARY_SOURCE <- ifelse(grepl('^G',gsp_inters_pws$PRIMARY_SOURCE),'GW','SW')
+#### this creates a weighted count of violations and of people served by gsp
 
-gsa_inters_pws %>% group_by(GSA_ID) %>% 
+gsp_inters_pws %>% group_by(GSP_ID) %>% 
    summarise(
       pws_count = n(),
       service_count = sum(service_count_prop,na.rm = t),
       service_count_gw_source = sum(service_count_prop*{PRIMARY_SOURCE=='GW'}),
-             health_viol_count_2014_2018 = sum(HVIOL_PROP,na.rm = T)) %>%
+      health_viol_count_2014_2018 = sum(HVIOL_PROP,na.rm = T)) %>%
    mutate(prop_service_gw_source = service_count_gw_source/service_count)
