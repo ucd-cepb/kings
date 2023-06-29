5872eb1f9595e2bfe554e3cbcab3def4b1e8af15
E.J. Zufall <90878994+ezufall@users.noreply.github.com>
2023-06-28 17:41:22 -0700

    updating filenames, test data

--- 1_run_govnet_generation.R
+++ 1_run_govnet_generation.R
@@ -2,14 +2,14 @@ ret_path <- "/Users/elisemiller/miniconda3/envs/spacy_condaenv/bin/python"
 generate_phrases <- FALSE
 test_data <- FALSE
 mini_data <- T
-parse_from_file <- T
+parse_from_file <- F
 #mini_data uses only a sample of pages to make testing and editing go faster
 
 if(test_data==T){
    pages <- (c("Invalid_sentence. DWR and DOT collaborate with the GSA to implement CDFA regulations.", 
-             "Governor Smith advises and councils Benny Jordan. Only one organization exists. National_Conservation_Service and US_Dept_of_Ag are to work together on providing the GSA the appropriate regulation language. Yolo County is to send the GSA all correspondence related to the basin setting. They shall report to the Board.", 
-             "The CDC will collaborate with NASA on the project. NRA's partners will include the FBI and several other agencies. The GSA is to submit their plan to the consultant. When the NSA meets with organizations such as the SWRCB, it is to take care to copy them on all correspondence.", 
-             "If they partner together, the GDE plan must be documented. The GSAs may not outsource their work to any other organizations. Sacramento Water Board may work with other partners as deemed necessary. Davis City Council may decide to incorporate the recommendations of BCDC."))
+             "Incorrect sentence. Percy Willis advises Frankie. Governor Smith advises and councils Benny Jordan. Philip Johnson and Sarah McMurray meet and discuss with their collaborators at USDA. National_Conservation_Service and US_Dept_of_Ag are to work together on providing the GSA the appropriate regulation language. Yolo County is to send the GSA all correspondence related to the basin setting. They shall report to the Board.", 
+             "The CDC will collaborate with NASA and coordinate with SGMA on the project. The FBI and CIA agree to meet with several other agencies. The GSA is to submit their plan to the consultant. When the NSA meets with organizations such as the SWRCB, it is to take care to copy them on all correspondence.", 
+             "If they partner together, the GDE plan must be documented. The GSAs may not outsource their work to any other organizations. Sacramento Water Board may work with other partners and submit documentation as deemed necessary. Davis City Council may decide to incorporate the recommendations of BCDC."))
    file_ids <- c("A","B","B","B")
    unique_files <- unique(file_ids)
    parsed_filenames <- paste0("data_temp/test_prs",unique_files)
@@ -39,7 +39,15 @@ if(test_data==T){
    }
 }
 
-generate_network(ret_path, generate_phrases, pages, file_ids, parsed_filenames, 
-                 parse_from_file=F)
+if(mini_data==T | test_data==T){
+   nodeedge_filenames <- paste0("network_extr_test/",unique_files)
+   
+}else{
+   nodeedge_filenames <- paste0("network_extracts/",unique_files)
+   
+}
+
+generate_networks(ret_path, generate_phrases, pages, file_ids, parsed_filenames, 
+                 nodeedge_filenames, parse_from_file)
 
 
