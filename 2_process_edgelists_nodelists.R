6a4b2ceb486a85e55ab328e71a4931ae5a05c489
E.J. Zufall <90878994+ezufall@users.noreply.github.com>
2023-07-25 17:48:38 -0700

    began moving procedure to textnet

--- 2_process_edgelists_nodelists.R
+++ 2_process_edgelists_nodelists.R
@@ -5,58 +5,65 @@ library(sna)
 library(stringr)
 library(dplyr)
 
+###Section 1: Govscitbl####
 source('govscicleaning.R')
 
-edges_and_nodes <- list.files(path = "network_extracts", full.names = T)
-gspids <- substr(edges_and_nodes, 18,21)
+govscitbl$State <- clean_entities(govscitbl$State, remove_nums = T)
 govscitbl$Agency <- clean_entities(govscitbl$Agency, remove_nums = T)
 govscitbl$Abbr <- clean_entities(govscitbl$Abbr, remove_nums = T)
 govscitbl <- unique(govscitbl)
 
-gsp_text_with_meta <- readRDS("data_output/gsp_docs_w_meta")
+
+###Section 2: GSAs####
+edges_and_nodes <- list.files(path = "network_extracts", full.names = T)
+gspids <- substr(edges_and_nodes, 18,21)
+
 agency_tbl <- readRDS(list.files(path = "data_output", pattern = "web_repaired", full.names = T)[
    length(list.files(path = "data_output", pattern = "web_repaired", full.names = T))])
 agency_tbl <- agency_tbl[!is.na(gsp_id),]
-
 #change hyphens and spaces to underscores, to match spacy parse formatting
 agency_tbl$name_gsas <- lapply(agency_tbl$name_gsas, function(w)
    stringr::str_replace_all(w,"-|\\s","_"))
 
+agency_nicknames <- setDT(list("name"=rep(vector(mode="list",length(edges_and_nodes)*2)),
+                               "nickname"=rep(NA_character_,length(edges_and_nodes)*2)))
 for(m in 1:length(edges_and_nodes)){
+   #remove parentheses
+   agency_names <- agency_tbl[gsp_id==gspids[m]]$name_gsas[[1]]
+   agency_names <- unlist(lapply(agency_names, function(b) str_split(b,"\\(")[[1]][1]))
+   #clean entities so they are same format as spacy tokens
+   agency_names <- clean_entities(agency_names, remove_nums=T)
+   plural <- agency_tbl[gsp_id==gspids[m]]$mult_gsas
    
+   if(plural==T){
+      agency_abbr <- c("Groundwater_Sustainability_Agencies","Agencies")
+   }else{
+      agency_abbr <- c("Groundwater_Sustainability_Agency","Agency")
+   }
+   agency_nicknames[m*2-1, (colnames(agency_nicknames)):= list(agency_names, agency_abbr[1])]
+   agency_nicknames[m*2, (colnames(agency_nicknames)):= list(agency_names, agency_abbr[2])]
+}
+
+###Section 3: Acronyms####
+
+#add GSAs -> Groundwater_Sustainability_Agencies
+###Section 4: Disambiguation####
+
+
+
+pdftxt <- readRDS("data_output/cleaned_pdfs")
+
+
+for(m in 1:length(edges_and_nodes)){
    edgenodelist <- readRDS(edges_and_nodes[m])
+   acrons <- find_acronyms(pdftxt[[m]])
+   customdt <- rbind(acrons,govscitbl,agency_nicknames[(m*2-1):(m*2)])
+   match_partial_entity <- rep(c(T,F,F), c(nrow(acrons),nrow(govscitbl),nrow(agency_nicknames[(m*2-1):(m*2)])))
+   prefixes <- list(c("^_*The_","^_*the_","^_*THE_","^_*The$","^_*the$","^_*THE$"), c("US_|U_S_|United_States_|UnitedStates"))
+   edgenodelist_dis <- disambiguate(from=customdt[,2], to=customdt[,1], match_partial_entity, edgenodelist, prefixes)
    
    #does not remove "California" prefixes
-   
-   #removing leading underscores, and US variations
-   edgelist <- as.data.frame(edgenodelist$edgelist)
-   edgelist$source <- str_remove(edgelist$source, "^_")
-   edgelist$source <- str_remove(edgelist$source, '^(US_|U\\.S\\._|United_States_|UnitedStates_)')
-   edgelist$target <- str_remove(edgelist$target, "^_")
-   edgelist$target <- str_remove(edgelist$target, '^(US_|U\\.S\\._|United_States_|UnitedStates_)')
-   nodelist <- as.data.frame(edgenodelist$nodelist)
-   nodelist$entity_cat <- str_remove(nodelist$entity_cat, "^_")
-   nodelist$entity_cat <- str_remove(nodelist$entity_cat, '^(US_|U\\.S\\._|United_States_|UnitedStates)')
-   #removing non-persons/gpes/orgs
-   edgelist <- edgelist %>% filter(source %in% nodelist$entity_cat & target %in% nodelist$entity_cat)
-   
-   agency_disambig <- function(strng,m){
-      #remove parentheses
-      agency_names <- agency_tbl[gsp_id==gspids[m]]$name_gsas[[1]]
-      agency_names <- unlist(lapply(agency_names, function(b) str_split(b,"\\(")[[1]][1]))
-      #clean entities so they are same format as spacy tokens
-      agency_names <- clean_entities(agency_names, remove_nums=T)
-      plural <- agency_tbl[gsp_id==gspids[m]]$mult_gsas
-      if((plural==T && !is.na(strng) && (tolower(strng) %in% c("the_gsas","the_agencies"))) | 
-         (!plural==T && is.na(strng) && (tolower(strng) %in% c("the_gsa","the_agency")))){
-         return(agency_names)
-      }else if (is.na(strng)){
-         return(NA_character_)
-      }else{
-         return(strng)
-      }
-   }
-   
+ 
    
    #TODO fix orgtyp bug
    #orgtyp <- function(strng){
@@ -68,53 +75,6 @@ for(m in 1:length(edges_and_nodes)){
    #}else
    # return(NA)
    #}
-   
-
-   #temp rows because changing character column into list column
-   edgelist$sourcetemp <- lapply(edgelist$source, function(strng) agency_disambig(strng,m))
-   edgelist$targettemp <- lapply(edgelist$target, function(strng) agency_disambig(strng,m))
-   edgelist$length_source <- sapply(edgelist$sourcetemp, length)
-   edgelist$length_target <- sapply(edgelist$targettemp, length)
-   rows <- edgelist[rep(seq(1, nrow(edgelist)), edgelist$length_source)]
-   rows$source <- unlist(edgelist$sourcetemp)
-   rowstarget <- rows[rep(seq(1, nrow(rows)), rows$length_target)]
-   rowstarget$target <- unlist(rows$targettemp)
-      
-   rowstarget$sourcetemp <- rowstarget$targettemp <- rowstarget$length_source <- rowstarget$length_target<- NULL
-   edgelist <- rowstarget
-   
-   nodelist$entity_cattemp <- lapply(nodelist$entity_cat, function(strng) agency_disambig(strng,m))
-   nodelist$length_entitycat <- sapply(nodelist$entity_cattemp, length)
-   rows <- nodelist[rep(seq(1, nrow(nodelist)), nodelist$length_entitycat)]
-   rows$entity_cat <- unlist(nodelist$entity_cattemp)
-   rows$entity_cattemp <- rows$length_entitycat <- NULL
-   nodelist <- rows
- 
-   remove_the <- function(v){
-      remove <- c("^_*The_","^_*the_","^_*THE_","^_*The$","^_*the$","^_*THE$")
-      index <- which(grepl(paste(remove,collapse = '|'),v,perl = T))
-      v[index] <- str_remove_all(v[index],paste(remove,collapse = '|'))
-      return(v)
-   }
-   
-   edgelist$source <- remove_the(edgelist$source)
-   edgelist$target <- remove_the(edgelist$target)
-   nodelist$entity_cat <- remove_the(nodelist$entity_cat)  
-   
-   abbr <- function(strng){
-      
-      if (!identical(grep(paste0("\b",strng,"\b"),govscitbl$Abbr,useBytes = F), integer(0))){
-         return(govscitbl$Agency[grep(paste0("\b",strng,"\b"),govscitbl$Abbr, useBytes = F)] )
-      }
-      else
-         return(strng)
-   }
-   
-   edgelist$source <- sapply(edgelist$source, abbr)
-   edgelist$target <- sapply(edgelist$target, abbr)
-   nodelist$entity_cat <- sapply(nodelist$entity_cat, abbr)
-   
-
    
    colnames(nodelist)[3] <- "num_appearances"
    nodelist <- nodelist %>% arrange(desc(num_appearances))
@@ -134,6 +94,8 @@ for(m in 1:length(edges_and_nodes)){
    #putting source and target first
    edgelist <- edgelist[,c(2:ncol(edgelist),1)]
    
+   
+   ###Section 5: Network Object Generation####
    #use graph_from_data_frame because you can put node list as an argument
    full_directed_graph <- igraph::graph_from_data_frame(edgelist, vertices = nodelist, directed = T)
    
