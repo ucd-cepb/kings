bbeebb0bff276b477580c9ac1fae2d36e6431096
E.J. Zufall <90878994+ezufall@users.noreply.github.com>
2023-08-31 18:32:17 -0700

    initial logit and correlation tests on verb class prevalence

--- code/verb_analysis_workflow/verb_analysis.R
+++ code/verb_analysis_workflow/verb_analysis.R
@@ -1,14 +1,16 @@
 library(data.table)
 library(stringr)
 library(dplyr)
-
-file_list <- list.files(path = "cleaned_extracts", full.names = T)
-gspids <- substr(file_list, 18,21)
+#important! If you want to do any additional filtering of the dataset, please 
+#DO NOT edit the cleaned_extracts file. 
+#Instead, save the filtered dataset in a different folder
+file_list <- list.files(path = "data/cleaned_extracts", full.names = T)
+gspids <- unname(sapply(file_list, function(k) gsub("\\D", "", k)))
 
 verblist <- vector(mode="list",length=length(file_list))
 for(m in 1:length(file_list)){
    if(!gspids[m] %in% c("0053","0089")){
-      verblist[[m]] <- readRDS(paste0("cleaned_extracts/",gspids[m],".RDS"))$verblist
+      verblist[[m]] <- readRDS(paste0("data/cleaned_extracts/",gspids[m],".RDS"))$verblist
    }
 }
 vlist <- rbindlist(verblist)
@@ -19,14 +21,14 @@ vlist <- vlist[!str_detect(vlist$head_verb_lemma,"[0-9]")]
 edgelist <- vector(mode="list",length=length(file_list))
 for(m in 1:length(file_list)){
    if(!gspids[m] %in% c("0053","0089")){
-      edgelist[[m]] <- readRDS(paste0("cleaned_extracts/",gspids[m],".RDS"))$edgelist
+      edgelist[[m]] <- readRDS(paste0("data/cleaned_extracts/",gspids[m],".RDS"))$edgelist
    }
 }
 
 nodelist <- vector(mode="list",length=length(file_list))
 for(m in 1:length(file_list)){
    if(!gspids[m] %in% c("0053","0089")){
-      nodelist[[m]] <- readRDS(paste0("cleaned_extracts/",gspids[m],".RDS"))$nodelist
+      nodelist[[m]] <- readRDS(paste0("data/cleaned_extracts/",gspids[m],".RDS"))$nodelist
    }
 }
 
@@ -44,35 +46,155 @@ elist <- rbindlist(edgelist)
 evlist <- left_join(elist, vlist)
 
 #dummy out verbnet types
-all_type_ids <- unique(unlist(evlist$type_id))
+all_type_ids <- as.character(sort(unique(as.numeric(unlist(evlist$type_id)))))
+type_dummies <- setNames(data.table(matrix(nrow = nrow(evlist), ncol = length(all_type_ids))), paste0("type_",all_type_ids))
 
-type_dummies <- setDT(key=all_type_ids)
-type_dummies[i,] <- lapply(evlist$type_id, function (i) t(sapply(all_type_ids, 
-                                 function(j) j %in% i)))
-type_dummies <- lapply(evlist$type_id, function (i) t(sapply(all_type_ids, 
-       function(j) j %in% i)))
-type_dms <- rbind(type_dummies)
-type_dmms <- setDT(type_dms)
+for(i in 1:nrow(evlist)){
+   type_dummies[i,names(type_dummies):= lapply(all_type_ids, 
+                              function(j) j %in% evlist$type_id[[i]])]
+}
 
-colnames(type_dms) <- paste0("type_",unique(unlist(evlist$head_lemma_type_id)))
+saveRDS(type_dummies, "data/temp_large_files/verb_type_dummies_for_edgelist")
+edgelist_with_vclass <- cbind(evlist, type_dummies)
+
+edgelist_with_vclass$gsp_id <- unlist(lapply(edgelist_with_vclass$doc_sent_verb, 
+                                      function(j) gsub(".*?([0-9]+).*", "\\1", j)))
+saveRDS(edgelist_with_vclass, "data/output_large_files/edgelist_with_verb_class")
 
 #join on gsp_mini for approval, etc
 
-
-
 #vars: source_entity_type, target_entity_type
 #approval
 #drinking water and ag variables
-
-
-
-#vars: source_entity_type, target_entity_type
-#approval
-#drinking water and ag variables
-gsp_meta <- readRDS("data_output/gsp_docs_w_meta")
-gsp_mini <- unique(gsp_meta[,c(14,16,19:26,7)])
+gsp_meta <- readRDS("data/output_large_files/gsp_docs_lean")
+gsp_mini <- unique(gsp_meta[,c("gsp_id","approval","mult_gsas",
+                               "fract_of_area_in_habitat_log_scaled",
+                               "urbangw_af_log_scaled",
+                               "gwsum",
+                               "percent_dac_by_pop_scaled",
+                               "Republican_Vote_Share_scaled",
+                               "Agr_Share_Of_GDP_scaled",
+                               "Perc_Bach_Degree_Over25_scaled",
+                               "local_govs_per_10k_people_log_scaled",
+                               "maxdryspell_scaled")])
 gsp_mini <- gsp_mini[!gsp_mini$gsp_id %in% c("0089","0053"),]
 #for meta
 #network_properties <- as_tibble(evlist)
-net_with_gsa_attr <- merge(gsp_mini, evlist)
-   
+edgelist_w_meta <- merge(gsp_mini, edgelist_with_vclass)
+saveRDS(edgelist_w_meta, "data/output_large_files/edgelist_with_verb_meta")
+
+#VERB CLASSES AND AG
+#summary(glm(type_64 ~ Agr_Share_Of_GDP_scaled + Republican_Vote_Share_scaled, data = edgelist_w_meta, family = "binomial"))
+#class 64 ~ "allow_verbs"
+#should remove word "include" and "forbid" type words
+
+#VERB CLASSES AND DW
+summary(glm(type_76 ~ Agr_Share_Of_GDP_scaled + urbangw_af_log_scaled + fract_of_area_in_habitat_log_scaled +
+               maxdryspell_scaled + gwsum, data = edgelist_w_meta, family = "binomial"))
+#class 76 limit_verbs #water restraint to protect drinking water
+#hypoth confirmed for ag and gwsum, not for urban, not for habitat, not for maxdryspell
+summary(glm(type_90 ~ Agr_Share_Of_GDP_scaled + urbangw_af_log_scaled + fract_of_area_in_habitat_log_scaled +
+               maxdryspell_scaled + gwsum, data = edgelist_w_meta, family = "binomial"))
+#class 90 exceed_verbs
+#hypoth confirmed for maxdryspell, not for gwsum, habitat, urban, or ag
+
+#VERB CLASSES AND GDEs
+#class 26 verbs_of_creation_and_transformation (mostly "develop")
+#class 76 limit_verbs #water restraint to protect drinking water
+#(see above)
+#class 90 exceed_verbs
+#(see above)
+
+#VERB CLASSES AND mult_gsas
+#class 13 verbs_of_change_of_possession
+summary(glm(type_13 ~ mult_gsas, data = edgelist_w_meta, family = "binomial"))
+#hypothesis not confirmed
+
+#class 14 learn_verbs
+summary(glm(type_14 ~ mult_gsas, data = edgelist_w_meta, family = "binomial"))
+#not confirmed
+
+#class 16 verbs_of_concealment
+summary(glm(type_16 ~ mult_gsas, data = edgelist_w_meta, family = "binomial"))
+#hypothesis confirmed
+
+#class 22 verbs_of_combining_and_attaching
+summary(glm(type_22 ~ mult_gsas, data = edgelist_w_meta, family = "binomial"))
+#not confirmed
+
+#class 23 verbs_of_separating_and_disassembling
+summary(glm(type_23 ~ mult_gsas, data = edgelist_w_meta, family = "binomial"))
+#hypothesis confirmed, although most common word is "vary"
+
+#class 36 verbs_of_social_interaction, eg "communicate", "collaborate", "correspond", "cooperate"
+summary(glm(type_36 ~ mult_gsas, data = edgelist_w_meta, family = "binomial"))
+#hypothesis confirmed!
+
+#class 37 verbs_of_communication
+summary(glm(type_37 ~ mult_gsas, data = edgelist_w_meta, family = "binomial"))
+#hypothesis confirmed!
+
+#class 58 verbs_of_urging_and_begging
+summary(glm(type_58 ~ mult_gsas, data = edgelist_w_meta, family = "binomial"))
+#hypothesis confirmed! Main verb is "require"
+
+#class 70 rely_verbs
+summary(glm(type_70 ~ mult_gsas, data = edgelist_w_meta, family = "binomial"))
+
+#class 71 conspire_verbs (main verb is collaborate. also includes "partner" but collaborate is covered elsewhere)
+
+#class 72 help_verbs
+summary(glm(type_72 ~ mult_gsas, data = edgelist_w_meta, family = "binomial"))
+#hypothesis confirmed!
+
+#class 73 cooperate_verbs
+summary(glm(type_73 ~ mult_gsas, data = edgelist_w_meta, family = "binomial"))
+
+#class 107 involve_verbs #main verb is include
+summary(glm(type_107 ~ mult_gsas, data = edgelist_w_meta, family = "binomial"))
+#hypothesis confirmed!
+
+#VERB CLASSES AND approval
+#has_hedge and is_future #confidence, planning
+#class 61 ~ "try_verbs"
+summary(glm(type_61 ~ as.factor(approval), data = edgelist_w_meta, family = "binomial"))
+#unapproved plans "try"/"attempt" more!
+
+#class 14 learn_verbs #learning
+summary(glm(type_14 ~ as.factor(approval), data = edgelist_w_meta, family = "binomial"))
+#no clear trend
+
+#class 26 verbs_of_creation_and_transformation #landscape transformation
+summary(glm(type_26 ~ as.factor(approval), data = edgelist_w_meta, family = "binomial"))
+#no clear trend
+
+#class 30 verbs_of_perception #psych elements (main verb is meet. would need to remove "meet")
+
+#class 31 psych-verbs-verbs_of_psychological_state #psych elements (main verbs are support, approve, affect, recharge)
+summary(glm(type_31 ~ as.factor(approval), data = edgelist_w_meta, family = "binomial"))
+#approved plans have this most. maybe because of discussion of "recharge"?
+
+#class 33 judgment_verbs #evaluation (main verbs = report, approve, recommend)
+summary(glm(type_33 ~ as.factor(approval), data = edgelist_w_meta, family = "binomial"))
+#no clear trend
+
+#class 34 verbs_of_assessment #thoroughness (main verbs = monitor, estimate, follow)
+summary(glm(type_34 ~ as.factor(approval), data = edgelist_w_meta, family = "binomial"))
+#hypothesis confirmed!
+
+#class 35 verbs_of_searching #thoroughness (main verb = monitor)
+summary(glm(type_35 ~ as.factor(approval), data = edgelist_w_meta, family = "binomial"))
+#hypothesis not confirmed! Weird -- do rejected plans discuss monitoring more?
+
+#class 45 verbs_of_change_of_state #transformation (main verbs = set, collect, operate, reduce)
+summary(glm(type_45 ~ as.factor(approval), data = edgelist_w_meta, family = "binomial"))
+#hypothesis confirmed!
+
+#class 54 measure_verbs #thoroughness (main verbs = estimate, contain, hold, serve, take, assess)
+summary(glm(type_54 ~ as.factor(approval), data = edgelist_w_meta, family = "binomial"))
+#hypothesis confirmed!
+
+#class 87 verbs_of_focusing_and_comprehending #thoroughness; main verb is "follow"
+summary(glm(type_87 ~ as.factor(approval), data = edgelist_w_meta, family = "binomial"))
+#hypothesis confirmed!
+
