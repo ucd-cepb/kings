03b8e7f15324f2da755dc0f0f889832fb36e0fa8
E.J. Zufall <90878994+ezufall@users.noreply.github.com>
2023-09-06 23:42:49 -0700

    workflow setup

---
+++ code/plan_evolution_workflow/plan_evo_analysis.R
@@ -0,0 +1,16 @@
+#after scraping, analyze:
+
+#how do the networks change over time? This would involve running the textNet
+#functions parse(), then textnet_extract(), then find_acronyms() and disambiguate(),
+#then using a tergm to analyze the changes over time
+
+#how to predict network-level attribute (approval) based on network attribute differences?
+#ask experts about this
+
+#read in determination letters and compare their content (through STM or 
+#"textnets" graph, which finds occurrence of terms in a document https://textnets.readthedocs.io/en/stable/tutorial.html
+#not "textNet" graph, which finds occurrence of entities in a sentence) )
+#to the basin-level attributes 
+
+#compare determination letter content to network-level attributes of the corresponding GSP plan version entity network (made through textNet)
+
---
+++ code/plan_evolution_workflow/sgma_plan_evo_webscraper.R
@@ -0,0 +1,8 @@
+#this should be a mod to the sgma_web_scraper that collects 
+#each version of the plan 
+#as well as the determination letters
+#and figures out whether each version was approved or rejected
+#based on whether there is a later version than the one in question
+#(which means the version in question was rejected)
+#or if not, what the current approval status states
+
