8fe15faa51e30c58c36f2df34ea65a220d5a804b
E.J. Zufall <90878994+ezufall@users.noreply.github.com>
2023-10-10 00:39:59 -0700

    removing scrap files

--- code/tests_and_scraps/renaming_nodes_scrap.R
+++
@@ -1,5 +0,0 @@
-supernodesdt <- supernodesdt[!duplicated(supernodesdt, by=c("name","entity_type")),]
-supernodesdt <- supernodesdt[,index := 1:.N,by="name"]
-supernodesdt <- supernodesdt[,`:=` (dups, ifelse(any(index>1),T,F)), by = "name"]
-supernodesdt <- supernodesdt[,`:=` (name, ifelse(dups==T,paste0(name,"#",index),name))]
-s
