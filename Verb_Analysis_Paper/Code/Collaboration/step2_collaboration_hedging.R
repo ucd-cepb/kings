c1139cc016f33864799334a47e1a0d9bfc51b8fe
E.J. Zufall <90878994+ezufall@users.noreply.github.com>
2023-11-06 18:56:21 -0800

    added meeting notes for future planning

--- Verb_Analysis_Paper/Code/Collaboration/step2_collaboration_hedging.R
+++ Verb_Analysis_Paper/Code/Collaboration/step2_collaboration_hedging.R
@@ -15,3 +15,15 @@ edgelist_w_meta <- readRDS(filekey[filekey$var_name=="edgelist_w_meta",]$filepat
 #do the most central nodes have more hedged edges attached to them than is typical in the network?
 #this may require an ergm.
 
+#notes from meeting with Tyler
+#what is the role of plans?
+#symbolic? signalling? is it a roadmap? 
+#if symbolic or signalling, important to say what other people should do
+#if its a roadmap you might want to hedge your actions
+#PAR would be a good place for the collaboration paper
+#PAR has a "takeaways for practitioners" section
+#next step = make an outline. Tag Tyler at rationale of why this matters, 
+#Elise first cut of what we did
+hedged ~ centrality
+
+
