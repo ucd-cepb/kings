0e37021ffea7111131738b16bf9e5fc5e62134d3
Aaron Guerra <aaronguerra501@gmail.com>
2023-12-06 12:48:18 -0800

    textNet test

--- .Rprofile
+++
@@ -1,1 +0,0 @@
-Sys.setenv(RETICULATE_PYTHON="/Users/elisemiller/miniconda3/envs/spacy_condaenv/bin/python")
--- EJ_Paper/Code/textNet_test.r
+++
@@ -1,8 +0,0 @@
-library(textNet)
-pdfs <- c(list.files(path = ".", pattern = "old.pdf", full.names = T),
-          list.files(path = ".", pattern = "new.pdf", full.names = T))
-ocr <- F
-maxchar <- 10000
-old_new_text <- textNet::pdf_clean(pdfs, keep_pages=T, ocr=F, maxchar=10000,
-                                   export_paths=NULL, return_to_memory=T, suppressWarn = F)
-names(old_new_text) <- c("old","new")
