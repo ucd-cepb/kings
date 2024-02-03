390fe0fee40a34aa57314639655f88694dfeeb97
Tyler Scott <tascott@ucdavis.edu>
2024-02-03 08:33:19 -0800

    loop org read in

--- EJ_Paper/Code/org_coding.R
+++ EJ_Paper/Code/org_coding.R
@@ -1,6 +1,34 @@
-n = readRDS('data/Text_Gov_Network_Methods_Paper/cleaned_extracts/0013.RDS')
-nodes = n$nodelist
-nodes = nodes[nodes$entity_type!='PERSON',]
+library(data.table)
+### set up a symbolic link
+### on. mac this is > ln -s [Box Location] [Github Location]
+### on windows, who knows
+### can always hardwire if you have to
+file_loc <- 'data/Text_Gov_Network_Methods_Paper/cleaned_extracts/'
+extract_list = list.files(file_loc)
+
+### convenience function for reading in and processing nodes
+### extracts have ORG, PERSON, and GPE nodetypes
+grab_orgs <- function(file,drop = 'PERSON'){
+   # read in rds file
+   temp = readRDS(file)
+   # isolate nodelist
+   nodes = temp$nodelist
+   # filter out node types we don't want
+   nodes = nodes[!nodes$entity_type%in% drop,]
+   return(nodes)
+}
+
+empty_node_dt <- data.table()
+
+### this is test reads in only first five!!!
+### remove the index to read in all
+for(file in extract_list[1:5]){
+   print(file)
+   temp = grab_orgs(paste0(file_loc,file))
+   empty_node_dt <- rbind(empty_node_dt,temp,fill = T,use.names = T)
+   empty_node_dt = empty_node_dt[,list(num_appearances = sum(num_appearances)),by=.(entity_name,entity_type)]
+}
+
 
 nodes$org_type <- NA
 nodes$CBO <- NA
