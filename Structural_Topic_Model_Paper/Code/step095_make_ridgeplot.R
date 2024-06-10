fab73f9caa6f087cc9e4b9b9d74221a4798b0e9d
E.J. Zufall <90878994+ezufall@users.noreply.github.com>
2024-06-10 12:33:54 -0700

    housekeeping and update to June 2024 model

---
+++ Structural_Topic_Model_Paper/Code/step095_make_ridgeplot.R
@@ -0,0 +1,34 @@
+
+
+library(stm)
+library(data.table)
+library(stringr)
+filekey <- read.csv("filekey.csv")
+
+tps <- readRDS(filekey[filekey$var_name=="topic_prevalence",]$filepath)
+
+tps$CC <- rowSums(tps[,topic_ids[["cc"]]])
+tps$DW <- rowSums(tps[,topic_ids[["dw"]]])
+tps$EJ <- rowSums(tps[,topic_ids[["ej"]]])
+tps$GDE <- rowSums(tps[,topic_ids[["gde"]]])
+tps$gsp_id <- rownames(tps)
+tps_unitopics <- tps[,c("CC","DW","EJ","GDE","gsp_id")]
+tps_unitopics <- pivot_longer(tps_unitopics,
+                              c("CC","DW","EJ","GDE"),
+                              names_to = "category", values_to = "proportion")
+tps_unitopics$category <- factor(tps_unitopics$category)
+library(ggridges)
+library(ggplot2)
+library(viridis)
+ridgeplot <- ggplot(tps_unitopics, aes(x = proportion, y = category, fill = category)) +
+   geom_density_ridges() +
+   scale_fill_manual(values = viridis(4)) +
+   theme_ridges() +
+   labs(title = 'Proportion of GSP Devoted to Each Topic Category')  +
+   theme(
+      legend.position="none",
+      panel.spacing = unit(0.1, "lines"),
+      strip.text.x = element_text(size = 8)
+   )
+ggsave(plot = ridgeplot,filename = "ridgeplot.png",dpi = 450,width = 7,height = 4,units = 'in')
+
--- Structural_Topic_Model_Paper/Code/step12_ind_var_cor_tests.R
+++ Structural_Topic_Model_Paper/Code/step12_ind_var_cor_tests.R
@@ -6,51 +6,50 @@ inputspattern <- inputsfilenamesplits[length(inputsfilenamesplits)]
 inputs <- readRDS(list.files(path = inputspath, pattern = inputspattern, full.names = T)[
    length(list.files(path = inputspath, pattern = inputspattern, full.names = T))])
 
-newscrapefilename <- filekey[filekey$var_name=="gsp_web_vars_planevolutionpaper",]$filepath
-newscrapefilenamesplits <- unlist(strsplit(newscrapefilename,split="/"))
-newscrapepath <- paste(newscrapefilenamesplits[1:(length(newscrapefilenamesplits)-1)],collapse = "/")
-newscrapepattern <- newscrapefilenamesplits[length(newscrapefilenamesplits)]
-newscrape <- readRDS(list.files(newscrapepath,pattern=newscrapepattern,full.names=T)[length(
-   list.files(newscrapepath,pattern=newscrapepattern))])
+#No longer necessary with new gsp_text_with_meta file from May 2024
+#newscrapefilename <- filekey[filekey$var_name=="gsp_web_vars_planevolutionpaper",]$filepath
+#newscrapefilenamesplits <- unlist(strsplit(newscrapefilename,split="/"))
+#newscrapepath <- paste(newscrapefilenamesplits[1:(length(newscrapefilenamesplits)-1)],collapse = "/")
+#newscrapepattern <- newscrapefilenamesplits[length(newscrapefilenamesplits)]
+#newscrape <- readRDS(list.files(newscrapepath,pattern=newscrapepattern,full.names=T)[length(
+#   list.files(newscrapepath,pattern=newscrapepattern))])
 
-newscrape <- newscrape[newscrape$version==1,]
+#newscrape <- newscrape[newscrape$version==1,]
 
-newscrape <- as.data.frame(cbind(gsp_id = newscrape$gsp_num_id, repairedapproval = newscrape$version_approval))
-mymeta <- dplyr::left_join(inputs$meta, newscrape)
-minimeta <- unique(mymeta[,c('gsp_id', 'urbangw_af_log_scaled','percent_dac_by_pop_scaled',
-                             'fract_of_area_in_habitat_log_scaled',
-                             'maxdryspell_scaled',
-                             'Agr_Share_Of_GDP_scaled',
-                             'Republican_Vote_Share_scaled',
-                             'Perc_Bach_Degree_Over25_scaled',
-                             'local_govs_per_10k_people_log_scaled',
+#newscrape <- as.data.frame(cbind(gsp_id = newscrape$gsp_num_id, repairedapproval = newscrape$version_approval))
+#mymeta <- dplyr::left_join(inputs$meta, newscrape)
+mymeta <- inputs$meta
+
+mymeta$priority_categorylow_or_verylow <- mymeta$priority_category == "low_or_verylow"
+mymeta$priority_categorymed <- mymeta$priority_category == "med"
+
+mymeta <- mymeta[,c('gsp_id', 'priority_categorylow_or_verylow',
+                             'priority_categorymed',
                              'mult_gsas',
-                             'gwsum',
-                             'repairedapproval')])
-mymeta <- mymeta[,c('urbangw_af_log_scaled','percent_dac_by_pop_scaled',
-                    'fract_of_area_in_habitat_log_scaled',
-                    'maxdryspell_scaled',
-                    'Agr_Share_Of_GDP_scaled',
-                    'Republican_Vote_Share_scaled',
-                    'Perc_Bach_Degree_Over25_scaled',
-                    'local_govs_per_10k_people_log_scaled',
-                    'mult_gsas',
-                    'gwsum',
-                    'repairedapproval')]
-minimeta <- minimeta[,c('urbangw_af_log_scaled','percent_dac_by_pop_scaled',
-                          'fract_of_area_in_habitat_log_scaled',
-                          'maxdryspell_scaled',
-                          'Agr_Share_Of_GDP_scaled',
-                          'Republican_Vote_Share_scaled',
-                          'Perc_Bach_Degree_Over25_scaled',
-                          'local_govs_per_10k_people_log_scaled',
-                          'mult_gsas',
-                          'gwsum',
-                          'repairedapproval')]
-colnames(mymeta)[colnames(mymeta)=="repairedapproval"] <- "approval"
-mymeta$approval <- as.factor(mymeta$approval)
-colnames(minimeta)[colnames(minimeta)=="repairedapproval"] <- "approval"
-minimeta$approval <- as.factor(minimeta$approval)
+                             'basin_population_log_scaled',
+                             'dsci_scaled',
+                             'log_well_MCL_exceedance_count_by_log_pop_scaled',
+                             'percent_dac_by_pop_scaled',
+                             'fract_of_area_in_habitat_log_scaled',
+                             'Agr_Share_Of_GDP_scaled',
+                             'Republican_Vote_Share_scaled')]
+
+
+minimeta <- unique(mymeta[,c('gsp_id', 'priority_categorylow_or_verylow',
+                             'priority_categorymed',
+                             'mult_gsas',
+                             'basin_population_log_scaled',
+                             'dsci_scaled',
+                             'log_well_MCL_exceedance_count_by_log_pop_scaled',
+                             'percent_dac_by_pop_scaled',
+                             'fract_of_area_in_habitat_log_scaled',
+                             'Agr_Share_Of_GDP_scaled',
+                             'Republican_Vote_Share_scaled')])
+
+#colnames(mymeta)[colnames(mymeta)=="repairedapproval"] <- "approval"
+#mymeta$approval <- as.factor(mymeta$approval)
+#colnames(minimeta)[colnames(minimeta)=="repairedapproval"] <- "approval"
+#minimeta$approval <- as.factor(minimeta$approval)
 
 library(ggcorrplot)
 #pal <- c(scico(3, palette = "vik") )
@@ -60,8 +59,19 @@ library(ggcorrplot)
 #   ifelse(ceiling(max_corr*1000)%%5==0,0,(5-ceiling(max_corr*1000)%%5)/1000)
 #min_corr <- max_corr*-1
 
+corrplotbypage <- ggcorrplot(cor(mymeta[,c(
+   "mult_gsas",               
+   "priority_categorylow_or_verylow",                
+   "priority_categorymed",
+   "basin_population_log_scaled",                  
+   "dsci_scaled",                                   
+   "log_well_MCL_exceedance_count_by_log_pop_scaled",
+   "percent_dac_by_pop_scaled",                      
+   "fract_of_area_in_habitat_log_scaled",            
+   "Agr_Share_Of_GDP_scaled",                        
+   "Republican_Vote_Share_scaled"                   
 
-corrplotbypage <- ggcorrplot(cor(mymeta[,1:10]), type = "upper", lab = T)#+
+)]), type = "upper", lab = T)#+
 #   theme(axis.text.x = element_text(size = 20),  # Order: top, right, bottom, left
 #         axis.text.y = element_text(size = 20))+
 #   labs(title = "Correlation between Predictors",
@@ -78,11 +88,25 @@ corrplotbypage <- ggcorrplot(cor(mymeta[,1:10]), type = "upper", lab = T)#+
 #   theme(legend.key=element_rect(colour="black",fill="white",
 #                                 linetype="solid")) 
 
-corrplotbygsp <- ggcorrplot(cor(minimeta[,1:10]), type = "upper", lab = T)
+corrplotbygsp <- ggcorrplot(cor(minimeta[,c(
+                     "mult_gsas",               
+                     "priority_categorylow_or_verylow",                
+                     "priority_categorymed",
+                     "basin_population_log_scaled",                  
+                     "dsci_scaled",                                   
+                     "log_well_MCL_exceedance_count_by_log_pop_scaled",
+                     "percent_dac_by_pop_scaled",                      
+                     "fract_of_area_in_habitat_log_scaled",            
+                     "Agr_Share_Of_GDP_scaled",                        
+                     "Republican_Vote_Share_scaled"                   
+                     
+                  )]), type = "upper", lab = T)
 
+saveRDS(corrplotbypage, paste0(filekey[filekey$var_name=="stmpaper_figures",]$filepath, "/corrplotbypage"))
 ggsave("predictor_predictor_corrplot_by_page.png",plot = corrplotbypage, device = "png", path = filekey[filekey$var_name=="stmpaper_figures",]$filepath,
-       width = 6030, height = 2835, dpi = 300, units = "px", bg = "white")
+       width = 6030, height = 3835, dpi = 400, units = "px", bg = "white")
 
-
+saveRDS(corrplotbygsp, paste0(filekey[filekey$var_name=="stmpaper_figures",]$filepath, "/corrplotbygsp"))
+        
 ggsave("predictor_predictor_corrplot_by_gsp.png",plot = corrplotbygsp, device = "png", path = filekey[filekey$var_name=="stmpaper_figures",]$filepath,
-       width = 6030, height = 2835, dpi = 300, units = "px", bg = "white")
+       width = 6030, height = 3835, dpi = 400, units = "px", bg = "white")
