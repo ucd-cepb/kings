05681b98d989368317a4307596f8d92ed8e3e4f7
Aaron Guerra <aaronguerra501@gmail.com>
2024-08-05 13:17:30 -0700

    more model testing

--- Policy_Instruments_Paper/Code/step_4_GSP_level_stats.R
+++ Policy_Instruments_Paper/Code/step_4_GSP_level_stats.R
@@ -3,8 +3,6 @@ library(ggraph)
 library(igraph)
 library(tidyverse)
 library(migraph)
-library(data.table)
-library(ggcorrplot)
 library(knitr)
 library(broom)
 
@@ -18,7 +16,7 @@ policies <- read_csv("Policy_Instruments_Paper/Data/policy_instruments_bruno.csv
 
 replace_letters <- function(x) { 
    x <- gsub('Y', 1, x)
-   x <- gsub('M', 1, x)
+   x <- gsub('M', 0.5, x)
    x <- gsub('N', 0, x)
    return(x)
 }
@@ -40,9 +38,18 @@ nets <- list()
 
 for (g in seq_along(gsp_ids)) {
    net <- readRDS(paste0(network_fp, "/", extract_list[g]))
+   
+   net <- set_vertex_attr(net,
+                          'core',
+                          value = node_core(net))
+   
    gsp_id <- gsp_ids[g]
    nets[[gsp_id]] <- net
-
+   
+   snet <- induced_subgraph(net, V(net)[GSA == 1])
+   corenet <- induced_subgraph(net, V(net)[core == 1])
+   govnet <- V(net)[V(net)$org_type %in% c('CA_Gov', 'Loc_Gov', 'GSA', 'NL_Gov', 'Spec_Dist')]
+   
    gsp_stats <- data.frame(
       gsp_id = as.numeric(gsp_id),
       n= vcount(net),
@@ -54,29 +61,50 @@ for (g in seq_along(gsp_ids)) {
       net_betweenness = migraph::network_betweenness(net),
       net_eigenvector = migraph::network_eigenvector(net),
       net_core = migraph::network_core(net),
+      net_fact = migraph::network_factions(net),
       net_reciprocity = migraph::network_reciprocity(net),
       net_transitivity = migraph::network_transitivity(net),
       net_assortativity = migraph::network_assortativity(net),
       net_tri = sum(igraph::triangles(net)),
-      net_eccentricity = max(igraph::eccentricity(net))
+      net_eccentricity = max(igraph::eccentricity(net)),
+      net_cent_bet = igraph::centr_betw(net)$centralization,
+      net_cent_deg = igraph::centr_degree(net)$centralization,
+      net_cent_eig = igraph::centr_eigen(net)$centralization,
+      
+      cnet_density = migraph::network_density(corenet),
+      cnet_diameter = migraph::network_diameter(corenet),
+      cnet_components = migraph::network_components(corenet),
+      cnet_degree = migraph::network_degree(corenet),
+      cnet_betweenness = migraph::network_betweenness(corenet),
+      cnet_eigenvector = migraph::network_eigenvector(corenet),
+      cnet_fact = migraph::network_factions(corenet),
+      cnet_reciprocity = migraph::network_reciprocity(corenet),
+      cnet_transitivity = migraph::network_transitivity(corenet),
+      cnet_assortativity = migraph::network_assortativity(corenet),
+      cnet_tri = sum(igraph::triangles(corenet)),
+      cnet_eccentricity = max(igraph::eccentricity(corenet)),
+      cnet_cent_clo = igraph::centr_clo(corenet)$centralization,
+      
+      org_types = length(unique(V(net)$org_type)),
+      gsa_deg = sum(degree(net, v=govnet, mode='out')) / ecount(net)
    )
    gsp_summary <- rbind(gsp_summary,gsp_stats)
    
    # remove isolates
    gnet <- delete.vertices(net, which(degree(net) == 0))
    
-   graph <- ggraph(gnet, layout = 'fr') +
-      geom_edge_link() +
-      geom_node_point(aes(color = org_type, size = degree, shape = as.factor(GSA))) +
-      theme_void() +
-      ggtitle(paste0("GSP ", gsp_id))+
-      theme_graph(background='white')
+   # graph <- ggraph(corenet, layout = 'fr') +
+   #    geom_edge_link() +
+   #    geom_node_point(aes(color = org_type, size = degree, shape = as.factor(GSA))) +
+   #    theme_void() +
+   #    ggtitle(paste0("GSP ", gsp_id))+
+   #    theme_graph(background='white')
    
-   ggsave(paste0("Policy_Instruments_Paper/Graphs/gsp_", gsp_id, ".png"), 
-          graph,
-          width = 9,
-          height = 9,
-          units = 'in')
+   # ggsave(paste0("Policy_Instruments_Paper/Graphs/gsp_", gsp_id, ".png"),
+   #        graph,
+   #        width = 9,
+   #        height = 9,
+   #        units = 'in')
 }
 
 gsp_summary <- tibble(gsp_summary)
@@ -88,52 +116,42 @@ merged <- gsp_summary %>%
 
 summary(merged)
 
+dep_vars <- names(merged)[2:34]; dep_vars
+
 # mods across all dependent variables
-for (var in names(merged)[4:16]){
+for (var in dep_vars){
    model <- glm(reformulate(var, response = 'allocations'), 
                 data = merged, 
                 family = binomial)
    print(summary(model))}
 
-for (var in names(merged)[4:16]){
+for (var in dep_vars){
    model <- glm(reformulate(var, response = 'trading'), 
-                data = merged, 
+                data = merged %>% filter(allocations==1), 
                 family = binomial)
    print(summary(model))}
 
-for (var in names(merged)[4:16]){
+for (var in dep_vars){
    model <- glm(reformulate(var, response = 'taxes_fees'), 
                 data = merged, 
                 family = binomial)
    print(summary(model))}
 
-for (var in names(merged)[4:16]){
+for (var in dep_vars){
    model <- glm(reformulate(var, response = 'pumping_restrictions'), 
                 data = merged, 
                 family = binomial)
    print(summary(model))}
 
-for (var in names(merged)[4:16]){
+for (var in dep_vars){
    model <- glm(reformulate(var, response = 'efficiency_incentives'), 
                 data = merged, 
                 family = binomial)
    print(summary(model))}
 
-for (var in names(merged)[4:16]){
+for (var in dep_vars){
    model <- glm(reformulate(var, response = 'any'), 
                 data = merged, 
                 family = binomial)
    print(summary(model))}
 
-
-for (i in seq_along(nets)) {
-   net <- nets[[i]]
-   t <- tibble(igraph::as_data_frame(net, what = 'vertices'))
-   print(gsp_ids[i])
-   print(t |> select(c(name, org_type, GSA)), n=20)
-   print("Press 'y' to continue: ")
-   response <- readline()
-   if (tolower(response) != 'y') {
-      break
-   }
-}
