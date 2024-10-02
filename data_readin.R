a2bdb71956f3a0393065dfb8bcb83727dd43260f
E.J. Zufall <90878994+ezufall@users.noreply.github.com>
2024-10-02 16:45:25 -0700

    Wendy onboarding

---
+++ data_readin.R
@@ -0,0 +1,12 @@
+filekey <- read.csv("filekey.csv")
+
+list_of_network_files <- list.files(filekey[filekey$var_name=="disambiged_unfiltered_extracts_instrumentpaper",]$filepath)
+
+network_files <- lapply(paste0(filekey[filekey$var_name=="disambiged_extracts_govnetpaper",]$filepath,"/",list_of_network_files),
+                               function (i) readRDS(i))
+
+gsp_meta <- read.csv(filekey[filekey$var_name == "gsp_planwise_metadata_csv",]$filepath)
+
+
+
+policies <- read.csv(filekey[filekey$var_name=="bruno_data",]$filepath) 
