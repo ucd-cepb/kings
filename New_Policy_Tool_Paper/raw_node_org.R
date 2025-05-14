4ad30122630f72f05e0cc9c2d304c03ced7c4b13
Wendy Song <wendysong@campus-056-016.ucdavis.edu>
2025-05-14 14:40:40 -0700

    export RDS with labeled node (including GPE and PERSON), export csv with name and core per plan, export statistics of each org_type distribution in core

--- New_Policy_Tool_Paper/raw_node_org.R
+++ New_Policy_Tool_Paper/raw_node_org.R
@@ -2,14 +2,18 @@ library(dotenv)
 library(ggraph)
 library(igraph)
 library(tidyverse)
-library(migraph)
+library(igraph)
 library(data.table)
 library(statnet)
 library(tidyverse)
 library(netUtils)
-
+library(network)
+library(sna)
+library(dplyr)
+library(tidyr)
 
 # first chunk Aaron part, the orgin of each dataset can be find in Structure(branch)/Network_structure/1_generate_networks
+# In addition to Aaron’s, replace NA in org_label if Entity_type is PERSON/GPE
 network_fp <- list.files(path = "New_Policy_Tool_Paper/Box_link/Network_policy_tool_paper/wendysong/cleaned_unfiltered_extracts", pattern = "*.RDS", full.names = TRUE)
 
 wl <- readLines("New_Policy_Tool_Paper/Box_link/Network_policy_tool_paper/wendysong/Node_label/wl.txt")
@@ -156,6 +160,11 @@ net_process <- function(file, gsp_id){
          org_type == 'Drop' ~ NA,
          TRUE ~ org_type
       )) %>%
+   ## Wendy Addition, Replace org_type NA if entity_type == GPE or PERSON
+         mutate(org_type = if_else(is.na(org_type) & entity_type %in% c("GPE", "PERSON"),
+                                entity_type,
+                                org_type)) %>%
+      
       select(-c(X, State, Abbr)) %>%
       distinct(., entity_name, .keep_all = TRUE)
 
@@ -169,7 +178,7 @@ net_process <- function(file, gsp_id){
 }
 
 # aggregate gsas (called in net_graph)
-net_graph <- function(networklist, gsp_id) {
+   net_graph <- function(networklist, gsp_id) {
    
    graph <- igraph::graph_from_data_frame(networklist$edgelist,
                                           vertices = networklist$nodelist)
@@ -313,21 +322,227 @@ for (g in seq_along(gsp_ids)) {
    gsp_graph <- net_graph(gsp_list, gsp_id = gsp_ids[g])
    
    
+   # save
+   filename <- basename(extract_list[g])
+   file = file.path("New_Policy_Tool_Paper/Box_link/Network_policy_tool_paper/wendysong/cleaned_extracts_org", filename)
+   saveRDS(object = gsp_graph$igraph,
+           file = file)
+   
    # remove isolates
    plot_graph <- delete_vertices(gsp_graph$igraph, which(igraph::degree(gsp_graph$igraph) == 0))
    
-   ggraph::ggraph(plot_graph, layout = 'fr') +
-      geom_edge_link(aes(edge_alpha = weight), show.legend = FALSE) +
-      geom_node_point(aes(color = org_type), size = 5) +
-      theme_void() +
-      ggtitle(paste0("GSP: ", gsp_id))
+   #ggraph::ggraph(plot_graph, layout = 'fr') +
+   #   geom_edge_link(aes(edge_alpha = weight), show.legend = FALSE) +
+   #   geom_node_point(aes(color = org_type), size = 5) +
+   #   theme_void() +
+   #   ggtitle(paste0("GSP: ", gsp_id))
 
-   ggsave(paste0("New_Policy_Tool_Paper/Box_link/Network_policy_tool_paper/wendysong/Node_label/graphs/", gsp_id, '.png'),
-       width = 9, height = 9, dpi = 300)
+   #ggsave(paste0("New_Policy_Tool_Paper/Box_link/Network_policy_tool_paper/wendysong/Node_label/graphs_na_adjust/", gsp_id, '.png'),
+   #    width = 9, height = 9, dpi = 300)
 
    print(paste0("Finished GSP ", gsp_id))
 }
 
 
-########## Wendy work after this ########
 
+########## Wendy: figure out whats NA ########
+
+## Use gsp_7 for testing ✅ 
+file_path <- "New_Policy_Tool_Paper/Box_link/Network_policy_tool_paper/wendysong/cleaned_extracts_org/0007.rds"
+demo <- readRDS(file_path)
+
+nodelist <- igraph::as_data_frame(demo, what = "vertices") %>%
+   select(name, org_type)
+
+edgelist <- igraph::as_data_frame(demo, what = "edges") %>%
+   select(from, to, weight) %>%
+   na.omit()
+
+# un-collapse since netUtils does not support weighted
+edgelist_expanded <- edgelist %>%
+   filter(weight >= 1) %>%
+   rowwise() %>%
+   mutate(repeat_list = list(rep(1, round(weight)))) %>%
+   unnest(repeat_list) %>%
+   select(from, to)
+
+# non_isolated
+all_nodes <- unique(c(edgelist$from, edgelist$to))
+nodelist <- nodelist[nodelist$name %in% all_nodes, ]
+
+g <- igraph::graph_from_data_frame(d = edgelist_expanded, vertices = nodelist, directed = TRUE)
+
+
+# Core–periphery analysis using netUtils::vec
+cp_fit <- netUtils::core_periphery(g)
+cp_fit$corr
+core_vec <- cp_fit$vec
+
+core_df <- tibble(
+   name = names(core_vec),
+   core = core_vec
+) %>%
+   left_join(nodelist, by = "name") %>%
+   select(name, org_type, core)
+print(head(core_df))
+
+
+## Start LOOOOOOOOOOOOPING 
+input_dir <- list.files(path = "New_Policy_Tool_Paper/Box_link/Network_policy_tool_paper/wendysong/cleaned_extracts_org/", pattern = "*.RDS", full.names = TRUE)
+output_dir <- "New_Policy_Tool_Paper/Box_link/Network_policy_tool_paper/wendysong/core_output_labeled/"
+dir.create(output_dir, showWarnings = FALSE, recursive = TRUE)
+
+
+for (i in 1:length(input_dir))  {
+   file_name <- tools::file_path_sans_ext(basename(input_dir[i]))
+   out_path <- file.path(output_dir, paste0(file_name, ".csv"))
+   
+   demo <- readRDS(input_dir[i])
+   
+   nodelist <- igraph::as_data_frame(demo, what = "vertices") %>%
+      select(name, org_type)
+   
+   edgelist <- igraph::as_data_frame(demo, what = "edges") %>%
+      select(from, to, weight) %>%
+      na.omit()
+   
+   edgelist_expanded <- edgelist %>%
+      filter(weight >= 1) %>%
+      rowwise() %>%
+      mutate(repeat_list = list(rep(1, round(weight)))) %>%
+      unnest(repeat_list) %>%
+      select(from, to)
+   
+   all_nodes <- unique(c(edgelist_expanded$from, edgelist_expanded$to))
+   nodelist_filtered <- nodelist[nodelist$name %in% all_nodes, ]
+   
+   g <- igraph::graph_from_data_frame(edgelist_expanded, vertices = nodelist_filtered, directed = TRUE)
+   
+   cp_fit <- netUtils::core_periphery(g)
+   core_vec <- cp_fit$vec
+   
+   core_df <- tibble(
+      name = names(core_vec),
+      core = core_vec
+   ) %>%
+      left_join(nodelist_filtered, by = "name") %>%
+      select(name, org_type, core)
+   
+   # Save to CSV
+   write.csv(core_df, out_path, row.names = FALSE)
+   message("✅ Processed: ", file_name)
+}
+
+
+
+###### Summary Statistics for Core
+
+input_dir <- "New_Policy_Tool_Paper/Box_link/Network_policy_tool_paper/wendysong/core_output_labeled/"
+csv_files <- list.files(input_dir, pattern = "\\.csv$", full.names = TRUE)
+
+all_summaries <- list()
+
+for (file in csv_files) {
+   gsp_id <- tools::file_path_sans_ext(basename(file))  # e.g., "0007"
+   df <- read_csv(file, show_col_types = FALSE)
+   
+   # Filter for core nodes
+   df_core <- df %>% filter(core == 1)
+   
+   # Summarize org_type counts
+   summary <- df_core %>%
+      count(org_type, name = "count") %>%
+      mutate(gsp_id = gsp_id) %>%
+      select(gsp_id, org_type, count)
+   
+   # Add names of NA org_type nodes
+   na_names <- df_core %>%
+      filter(is.na(org_type)) %>%
+      pull(name)
+   
+   if (length(na_names) > 0) {
+      summary <- summary %>%
+         mutate(na_node_names = ifelse(is.na(org_type), paste(na_names, collapse = "; "), NA))
+   } else {
+      summary <- summary %>%
+         mutate(na_node_names = NA)
+   }
+   
+   all_summaries[[gsp_id]] <- summary
+}
+
+combined_summary <- bind_rows(all_summaries)
+write_csv(combined_summary, file.path(output_dir, "core_node_summary_all.csv"))
+
+
+
+## Most Frequent NA org_type names
+output_dir <- "New_Policy_Tool_Paper/Box_link/Network_policy_tool_paper/wendysong/core_output_labeled/summary_statistics/"
+na_name_counts <- list()
+
+for (file in csv_files) {
+   df <- read_csv(file, show_col_types = FALSE)
+   
+   na_core_names <- df %>%
+      filter(core == 1, is.na(org_type)) %>%
+      pull(name)
+   
+   if (length(na_core_names) > 0) {
+      na_name_counts[[basename(file)]] <- na_core_names
+   }
+}
+
+all_na_names <- unlist(na_name_counts)
+na_name_summary <- as_tibble(table(all_na_names)) %>%
+   arrange(desc(n)) %>%
+   rename(name = all_na_names, frequency = n)
+
+write_csv(na_name_summary, file.path(output_dir, "na_orgtype_core_name_frequency.csv"))
+
+
+## percentage of each org_type that are core per plan
+
+summary_all <- list()
+
+for (file in csv_files) {
+   df <- read_csv(file, show_col_types = FALSE)
+   
+   gsp_id <- tools::file_path_sans_ext(basename(file))
+   
+   df_core <- df %>% filter(core == 1)
+   
+   if (nrow(df_core) == 0) next  
+   
+   total_core <- nrow(df_core)
+   
+   summary_df <- df_core %>%
+      count(org_type, name = "count_core") %>%
+      mutate(
+         gsp_id = gsp_id,
+         percent_core = round((count_core / total_core) * 100, 1)
+      ) %>%
+      select(gsp_id, org_type, percent_core)
+   
+   summary_all[[gsp_id]] <- summary_df
+}
+summary_table <- bind_rows(summary_all)
+write_csv(summary_table, file.path(output_dir, "core_orgtype_percent_by_plan.csv"))
+
+# Distribution of org_type in core among all plans
+all_core_nodes <- list()
+
+for (file in csv_files) {
+   df <- read_csv(file, show_col_types = FALSE)
+   df_core <- df %>% filter(core == 1)
+   
+   if (nrow(df_core) > 0) {
+      all_core_nodes[[file]] <- df_core
+   }
+}
+
+combined_core_df <- bind_rows(all_core_nodes)
+overall_summary <- combined_core_df %>%
+   count(org_type, name = "count_core") %>%
+   mutate(percent_core = round(100 * count_core / sum(count_core), 1)) %>%
+   arrange(desc(percent_core))
+write_csv(overall_summary, file.path(output_dir, "core_orgtype_percent_overall.csv"))
